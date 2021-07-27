2570bea6834644336f54852e93a1b6ced55e2e45
somebodyawesome <luckynoob@yahoo.com>
2021-07-28 00:55:43 +0100

    init packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import 'tailwindcss/tailwind.css'
 
 export default function Home() {
   return (

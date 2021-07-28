fefc9a1cb46f88292fcf7073e9e7a54ebc5fdcbb
somebodyawesome <luckynoob@yahoo.com>
2021-07-28 18:45:26 +0100

    adding component to home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        homeGrid: "1fr 1fr",
+      },
+      backgroundImage: (theme) => ({
+        "home-bg": "url('/homeBG.jpg')",
+      }),
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};

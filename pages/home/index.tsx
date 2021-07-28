8e474693e62b31b2bba92ea5769c226991296b79
Hamza ben Romdhane <mizou7amza@live.fr>
2021-07-28 15:07:11 +0100

    test

---
+++ pages/home/index.tsx
@@ -0,0 +1,10 @@
+import 'tailwindcss/tailwind.css'
+export default function home (){
+    return(
+        <div className="text-5xl font-extrabold">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+            Hello world
+        </span>
+        </div>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import 'tailwindcss/tailwind.css'
 
 export default function Home() {
   return (

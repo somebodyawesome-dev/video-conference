508597ec9434e971b5efbe33d67bd1474d5986ad
Berlom <mizou7amza@live.fr>
2021-07-31 13:26:44 +0100

    test

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -4,7 +4,7 @@ import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 type ChatProps = { toggleChat: boolean };
 export default function Chat({ toggleChat }: ChatProps) {
   const chatStyle =
-    " fixed flex flex-col justify-between box-border bg-gray-600 transition-all duration-700 h-1/4 w-full sm:h-full sm:w-2/5  md:w-2/6 lg:w-80 ";
+    " flex flex-col justify-between box-border bg-gray-600 transition-all duration-700 h-1/4 w-full sm:h-full sm:w-2/5  md:w-2/6 lg:w-80 ";
   const displayChat = "right-0 ";
   const hideChat = "right-full ";
   return (
@@ -16,11 +16,11 @@ export default function Chat({ toggleChat }: ChatProps) {
       {/* {chat input} */}
       <div className="m-2 p-2 flex box-border justify-center   ring-2  ring-white rounded-lg  focus-within:ring-blue-500">
         <input
-          className="outline-none  transition-all duration-700 w-full border-b-2 border-opacity-0 focus:border-opacity-70  bg-transparent"
+          className="outline-none  transition-all text-white duration-700 w-full border-b-2 border-opacity-0 focus:border-opacity-70  bg-transparent"
           placeholder="type something"
           type="text"
         />
-        <div className="rounded-lg mx-1 transition duration-500 box-border p-2 text-white  hover:bg-white hover:text-gray-700">
+        <div className="rounded-lg mx-1 transition duration-500 box-border p-2 text-white  hover:bg-white hover:text-gray-700 hover:cursor-pointer">
           <FontAwesomeIcon className="" icon={faPaperPlane}></FontAwesomeIcon>
         </div>
       </div>
--- components/VideoChat.tsx
+++ components/VideoChat.tsx
@@ -3,7 +3,7 @@ type VideoChatProps = {
 };
 export default function VideoChat({ onToggleChat }: VideoChatProps) {
   return (
-    <div className="flex-grow bg-gray-700">
+    <div className="flex-grow bg-gray-700 box-border transition-all duration-700 h-3/4 w-full sm:h-full sm:w-3/5  md:w-4/6 lg:w-80">
       video calls
       <button
         className="transition m-2 p-2 px-3 border-solid border-2 border-blue-500 rounded-lg hover:text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600 focus:ring-opacity-50"
